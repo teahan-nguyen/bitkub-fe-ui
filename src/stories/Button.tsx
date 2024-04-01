6db4101117af1a29a387f622830aa5d5c2c8eff2
Alan Ngo <hoangnv1@vmogroup.com>
2024-04-01 16:48:57 +0700

    feat/storybook-button-component

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,41 +1,56 @@
-/* eslint-disable */
-import React from "react";
+import StarIcon from "./assets/svgs/Star";
+import {
+  EButtonType,
+  ESize,
+} from "/home/vmo/Workspace/nextjs/bitkub-fe-ui/src/enums/index";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
+  variant?: "primary" | "secondary" | "warning" | "positive";
   size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
+  disabled?: boolean;
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
+const SIZE_MAPPING = {
+  [ESize.SMALL]: "w-32 h-8 px-4 py-1.5",
+  [ESize.MEDIUM]: "w-[136px] h-10 px-4 py-2",
+  [ESize.LARGE]: "w-[136px] h-11 px-4 py-2.5",
+};
+
+const COLOR_MAPPING = {
+  [EButtonType.PRIMARY]:
+    "bg-blue-700 text-white hover:enabled:bg-blue-500 active:bg-blue-900",
+  [EButtonType.SECONDARY]:
+    "bg-white border border-black hover:enabled:bg-blue-50 active:bg-blue-100",
+  [EButtonType.WARNING]:
+    "bg-red-600 text-white hover:enabled:bg-red-700 active:bg-red-800",
+  [EButtonType.POSITIVE]:
+    "bg-green-500 text-white hover:enabled:bg-green-600 active:bg-green-700",
+};
+
 export const Button = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
+  variant = EButtonType.PRIMARY,
+  size = ESize.MEDIUM,
   label,
-  ...props
+  disabled = false,
+  onClick,
 }: ButtonProps) => {
-  // const mode = primary
-  //   ? "storybook-button--primary"
-  //   : "storybook-button--secondary";
-  return <button {...props}>button</button>;
+  return (
+    <button
+      className={`${COLOR_MAPPING[variant]} ${SIZE_MAPPING[size]} ${disabled ? "opacity-50" : ""} rounded-lg flex font-semibold justify-between items-center`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <StarIcon
+        size={size === "small" ? "small" : "medium"}
+        fill={variant === "secondary" ? "black" : "white"}
+      />
+      {label}
+      <StarIcon
+        size={size === "small" ? "small" : "medium"}
+        fill={variant === "secondary" ? "black" : "white"}
+      />
+    </button>
+  );
 };
